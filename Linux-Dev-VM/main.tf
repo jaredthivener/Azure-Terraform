8011af122d5265023a647997d2651dae2eddc12a
Jared Thivener <jaredthivener@gmail.com>
2023-04-01 16:52:09 -0400

    updated output to display public ip

--- Linux-Dev-VM/main.tf
+++ Linux-Dev-VM/main.tf
@@ -144,3 +144,7 @@ data "azurerm_public_ip" "terraform-ip-data" {
   name                = azurerm_public_ip.terraform-ip.name
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.terraform-vm.public_ip_address
+}
