78f93aeb05973daf5200a70548ffb22539582e2d
Jared Thivener <jaredthivener@gmail.com>
2023-04-17 19:10:09 -0400

    updated aks config - ingress_appgw

--- aks-ingress-gw/main.tf
+++ aks-ingress-gw/main.tf
@@ -1,11 +1,19 @@
-variable "resource_group_name" {}
+provider "azurerm" {
+  features {}
+}
 
-variable "cluster_name" {}
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+  tags = {
+    Environment = "dev"
+  }
+}
 
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = var.cluster_name
-  location            = var.resource_group_name.location
-  resource_group_name = var.resource_group_name.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = var.cluster_name
 
   default_node_pool {
---
+++ aks-ingress-gw/variables.tf
@@ -0,0 +1,11 @@
+variable "rg_name" {
+  default = "rg-aks-terraform"
+}
+
+variable "location" {
+  default = "eastus2"
+}
+
+variable "cluster_name" {
+    default = "my-aks-cluster"
+}
